860bf787e44c7520bce7f79471e0575a24c503ab
nnhoangtrieu <107376082+nnhoangtrieu@users.noreply.github.com>
2023-12-20 21:45:50 +0700

    add val set

--- test.py
+++ test.py
@@ -1,23 +1,30 @@
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 
-# Create a figure with a 1x4 subplot grid
-fig = plt.figure(figsize=(50,50))
+# Create a figure and a 3D axis
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# Add a subplot at position 1
-ax1 = fig.add_subplot(141)
-ax1.plot([1, 2, 3], [4, 5, 6])
+# Coordinates
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 4, 5, 6]
+z = [0, 1, 2, 3, 4]
 
-# Add a subplot at position 2
-ax2 = fig.add_subplot(142)
-ax2.plot([1, 2, 3], [4, 5, 6])
+# Generate a list of colors for each data point
+colors = np.arange(len(x))
 
-# Add a subplot at position 3
-ax3 = fig.add_subplot(143)
-ax3.plot([1, 2, 3], [4, 5, 6])
+# Plot the points with unique colors
+scatter = ax.scatter(x, y, z, c=colors, cmap='viridis', marker='o')
 
-# Add a subplot at position 4
-ax4 = fig.add_subplot(144)
-ax4.plot([1, 2, 3], [4, 5, 6])
+# Set labels
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+# Add a colorbar to show the mapping of colors to values
+cbar = plt.colorbar(scatter)
+cbar.set_label('Color Index')
 
 # Show the plot
 plt.show()
