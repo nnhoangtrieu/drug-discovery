a832fab2f1482c597e15ccd957685380dda3dc33
nnhoangtrieu <107376082+nnhoangtrieu@users.noreply.github.com>
2023-12-21 13:52:42 +0700

    add method

--- test.py
+++ test.py
@@ -1,30 +1,5 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
+import selfies
 
-# Create a figure and a 3D axis
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-# Coordinates
-x = [1, 2, 3, 4, 5]
-y = [2, 3, 4, 5, 6]
-z = [0, 1, 2, 3, 4]
-
-# Generate a list of colors for each data point
-colors = np.arange(len(x))
-
-# Plot the points with unique colors
-scatter = ax.scatter(x, y, z, c=colors, cmap='viridis', marker='o')
-
-# Set labels
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
-
-# Add a colorbar to show the mapping of colors to values
-cbar = plt.colorbar(scatter)
-cbar.set_label('Color Index')
-
-# Show the plot
-plt.show()
+test = 'COc1ccc(cc1)/C=C/C(=O)N1CCCCC1'
+out = selfies.encoder(test)
+print(type(out))
